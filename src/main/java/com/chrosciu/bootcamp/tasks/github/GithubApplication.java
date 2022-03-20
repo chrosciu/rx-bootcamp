31fd8c60d6da51feec809418a7a741bbb9e1964f
Jacek Motyka <jmotyka9@gmail.com>
2022-03-20 22:48:18 +0100

    tasks solved

--- src/main/java/com/chrosciu/bootcamp/tasks/github/GithubApplication.java
+++ src/main/java/com/chrosciu/bootcamp/tasks/github/GithubApplication.java
@@ -1,10 +1,13 @@
 package com.chrosciu.bootcamp.tasks.github;
 
+import com.chrosciu.bootcamp.tasks.github.dto.Branch;
+import com.chrosciu.bootcamp.tasks.github.dto.Repository;
 import com.jakewharton.retrofit2.adapter.reactor.ReactorCallAdapterFactory;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import reactor.core.publisher.Flux;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -37,6 +40,30 @@ public class GithubApplication {
 
     @SneakyThrows
     private void run() {
+        //zadanie 1
+        Flux<Repository> flux1 = githubClient.getUserRepositories("Jacentus");
+        flux1.subscribe(i -> System.out.println(i),
+                error -> System.out.println("Error " + error),
+                () -> System.out.println("Completed successfully"));
+        System.out.println("Zadanie 2 ****************************************");
+        //zadanie 2
+        Flux<Branch> flux2 = githubClient.getUserRepositoryBranches("Jacentus", "rx-bootcamp");
+        flux2.subscribe(i -> System.out.println(i),
+                error -> System.out.println("Error " + error),
+                () -> System.out.println("Completed successfully"));
+        System.out.println("Zadanie 3 ****************************************");
+        //zadanie 3
+        Flux flux3 = Flux.just("Jacentus", "chrosciu");
+        Flux<Repository> flux4 = githubClient.getUsersRepositories(flux3);
+        flux4.subscribe(i -> System.out.println(i),
+                error -> System.out.println("Error " + error),
+                () -> System.out.println("Completed successfully"));
+        System.out.println("Zadanie 4 ****************************************");
+        //zadanie 4
+        githubClient.getAllUserBranchesNames("Jacentus")
+                .subscribe(i -> System.out.println(i),
+                        error -> System.out.println("Error " + error),
+                        () -> System.out.println("Completed successfully"));
     }
 
     public static void main(String[] args) {
--- src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
+++ src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
@@ -4,28 +4,41 @@ import com.chrosciu.bootcamp.tasks.github.dto.Branch;
 import com.chrosciu.bootcamp.tasks.github.dto.Repository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class GithubClient {
     private final GithubApi githubApi;
 
     public Flux<Repository> getUserRepositories(String username) {
-        //TODO: Implement
-        return null;
+        Mono<List<Repository>> mono = githubApi.getUserRepositories(username);
+        Flux<Repository> flux = mono.flatMapMany(Flux::fromIterable);
+        return flux;
     }
 
     public Flux<Branch> getUserRepositoryBranches(String username, String repo) {
-        //TODO: Implement
-        return null;
+
+        Mono<List<Branch>> mono = githubApi.getUserRepositoryBranches(username, repo);
+        Flux<Branch> flux = mono.flatMapMany(Flux::fromIterable);
+        return flux;
     }
 
     public Flux<Repository> getUsersRepositories(Flux<String> usernames) {
-        //TODO: Implement
-        return null;
+
+    /*  Flux<Repository> flux = usernames.flatMap(x -> getUserRepositories(x));
+        return flux;*/
+        return usernames.flatMap(user -> getUserRepositories(user));
     }
 
     public Flux<String> getAllUserBranchesNames(String username) {
-        //TODO: Implement
-        return null;
+
+        Flux<Repository> flux = getUserRepositories(username);
+        Flux<Branch> flux2 = flux.flatMap(x -> getUserRepositoryBranches(username, x.getName()));
+        Flux<String> flux3 = flux2.map(x -> x.getName());
+
+        return flux3;
     }
 }
