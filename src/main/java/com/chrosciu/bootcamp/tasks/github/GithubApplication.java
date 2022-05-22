f4b9feac9828b6d7ae10ae6905cd66cbde6171e6
Jacek Kitlinski <gasniczka@o2.pl>
2022-05-22 20:25:27 +0200

    reflux - homework

--- src/main/java/com/chrosciu/bootcamp/tasks/github/GithubApplication.java
+++ src/main/java/com/chrosciu/bootcamp/tasks/github/GithubApplication.java
@@ -1,12 +1,19 @@
 package com.chrosciu.bootcamp.tasks.github;
 
+import com.chrosciu.bootcamp.tasks.github.dto.Branch;
+import com.chrosciu.bootcamp.tasks.github.dto.Repository;
 import com.jakewharton.retrofit2.adapter.reactor.ReactorCallAdapterFactory;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
+import reactor.core.publisher.Flux;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+import static com.chrosciu.bootcamp.tasks.input.InputUtils.toFlux;
 
 @Slf4j
 public class GithubApplication {
@@ -37,6 +44,62 @@ public class GithubApplication {
 
     @SneakyThrows
     private void run() {
+
+        logHeader("START 1a");
+
+        Flux<Repository> githubRepository = githubClient.getUserRepositories("gasniczka");
+        githubRepository.subscribe(r -> log.info("{}", r));
+
+
+        logHeader("START 1c");
+
+        Flux<Branch> githubBranch = githubClient.getUserRepositoryBranches("gasniczka", "Sages_01_test_git");
+        githubBranch.subscribe(r -> log.info("{}", r));
+
+
+        logHeader("START 1b");
+
+        Flux<String> users = Flux.just("chrosciu", "gasniczka");
+        Flux<Repository> githubUsersRepositories = githubClient.getUsersRepositories(users);
+        githubUsersRepositories.subscribe(r -> log.info("{}", r));
+
+
+        logHeader("START 1d");
+
+        Flux<String> githubUserAllBranches = githubClient.getAllUserBranchesNames("gasniczka");
+        githubUserAllBranches.subscribe(r -> log.info("{}", r));
+
+
+        logHeader("START 3 - enter line of text, exxit -> exit procedure");
+
+        Flux<String> newInput = toFlux(System.in);
+        newInput.subscribe(r -> log.info("line: {}", r), e -> log.error("error: ", e));
+
+
+        logHeader("START 4 - enter github username, exxit -> exit procedure");
+        // pobieranie danych z wejscia
+
+        Flux<String> newInputUser = toFlux(System.in);
+        newInputUser.subscribe(r -> {
+
+            Flux<String> githubUserFromInAllBranches = githubClient.getAllUserBranchesNames(r);
+            githubUserFromInAllBranches.subscribe(br -> log.info("{}", br));
+
+        });
+
+
+        logHeader("START 5");
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        Flux<String> githubUserAllBranchesInParallel = githubClient.getAllUserBranchesNamesInParallel("gasniczka");
+        githubUserAllBranchesInParallel.subscribe(r -> log.info("{}", r), e -> log.error("error", e), () -> countDownLatch.countDown());
+
+        countDownLatch.await();
+
+        logHeader("END");
+
+
     }
 
     public static void main(String[] args) {
@@ -47,4 +110,10 @@ public class GithubApplication {
             githubApplication.dispose();
         }
     }
+
+
+    private static void logHeader(String message) {
+        log.info("\n\n\n##### {}", message);
+    }
+
 }
--- src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
+++ src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
@@ -4,28 +4,78 @@ import com.chrosciu.bootcamp.tasks.github.dto.Branch;
 import com.chrosciu.bootcamp.tasks.github.dto.Repository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.ParallelFlux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class GithubClient {
     private final GithubApi githubApi;
 
     public Flux<Repository> getUserRepositories(String username) {
-        //TODO: Implement
-        return null;
+
+        Mono<List<Repository>> repos = githubApi.getUserRepositories(username);
+        return repos.flatMapMany(Flux::fromIterable);
     }
 
     public Flux<Branch> getUserRepositoryBranches(String username, String repo) {
-        //TODO: Implement
-        return null;
+
+        Mono<List<Branch>> branches = githubApi.getUserRepositoryBranches(username, repo);
+        return branches.flatMapMany(Flux::fromIterable);
     }
 
     public Flux<Repository> getUsersRepositories(Flux<String> usernames) {
-        //TODO: Implement
-        return null;
+
+        // tu publisher z uzytkownikami
+        Flux<Repository> usersRepositories =
+                usernames
+                        .log()
+                        .flatMap(s -> githubApi.getUserRepositories(s))
+                        .flatMap(Flux::fromIterable);
+
+        return usersRepositories;
     }
 
     public Flux<String> getAllUserBranchesNames(String username) {
-        //TODO: Implement
-        return null;
+
+//        Mono<List<Repository>> repos = githubApi.getUserRepositories(username);
+//
+//        Flux<Repository> repositories = repos.flatMapMany(Flux::fromIterable);
+//
+//        Flux<String> branchesNames =
+//                repositories
+//                        .log()
+//                        .flatMap(repository -> githubApi.getUserRepositoryBranches(username, repository.getName()))
+//                        .flatMap(Flux::fromIterable)
+//                        .log()
+//                        .map(Branch::getName)
+//                        .log()
+//                        ;
+
+        return githubApi.getUserRepositories(username)
+                .flatMapMany(Flux::fromIterable)
+                .flatMap(repository -> githubApi.getUserRepositoryBranches(username, repository.getName()))
+                .flatMap(Flux::fromIterable)
+                .map(Branch::getName)
+                ;
     }
+
+
+    public Flux<String> getAllUserBranchesNamesInParallel(String username) {
+
+        return githubApi.getUserRepositories(username)
+                .flatMapMany(Flux::fromIterable)
+                .parallel(3)
+                .runOn(Schedulers.parallel())
+                .flatMap(repository -> githubApi.getUserRepositoryBranches(username, repository.getName()))
+                .flatMap(Flux::fromIterable)
+                .sequential()
+                //.publishOn(Schedulers.single())
+                .map(Branch::getName)
+                ;
+    }
+
+
 }
