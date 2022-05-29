f535a526c8e5c28a98e13e069af4b114332bdaa8
Piotr Baranowski <baranosiu@gmail.com>
2022-05-29 18:53:38 +0200

    Zrobione. Drobna refaktoryzacja (eliminacja zmiennych pośrednich)

--- src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
+++ src/main/java/com/chrosciu/bootcamp/tasks/github/GithubClient.java
@@ -3,13 +3,9 @@ package com.chrosciu.bootcamp.tasks.github;
 import com.chrosciu.bootcamp.tasks.github.dto.Branch;
 import com.chrosciu.bootcamp.tasks.github.dto.Repository;
 import lombok.RequiredArgsConstructor;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 public class GithubClient {
@@ -26,23 +22,20 @@ public class GithubClient {
     }
 
     public Flux<Repository> getUsersRepositories(Flux<String> usernames) {
-        Flux<Repository> flux = usernames.flatMap(this::getUserRepositories);
-        return flux;
+        return usernames.flatMap(this::getUserRepositories);
     }
 
     public Flux<String> getAllUserBranchesNames(String username) {
-        Flux<String> flux = getUserRepositories(username)
+        return getUserRepositories(username)
                 .flatMap(repo -> getUserRepositoryBranches(username, repo.getName()))
                 .map(Branch::getName);
-        return flux;
     }
 
     public Flux<String> getAllUserBranchesNamesParallel(String username) {
         Scheduler scheduler = Schedulers.boundedElastic();
-        Flux<String> flux = getUserRepositories(username)
+        return getUserRepositories(username)
                 .subscribeOn(scheduler)
                 .flatMap(repo -> getUserRepositoryBranches(username, repo.getName()))
                 .map(Branch::getName);
-        return flux;
     }
 }
